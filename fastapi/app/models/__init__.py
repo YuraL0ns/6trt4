12e24a7c62bd57a92bf6c45e5f17ec0f0e52a2d6
YuraL0ns <yuralons@ya.ru>
2025-12-28 16:45:48 +0000

    resolve

--- fastapi/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .event import Event
-from .photo import Photo
-
-__all__ = ["Event", "Photo"]
-
-
--- fastapi/run_celery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Скрипт для запуска Celery worker
-# Слушаем все очереди: celery (default), high_priority, default, low_priority
-
-celery -A tasks.celery_app worker --loglevel=info --concurrency=4 --queues=celery,high_priority,default,low_priority
-
-
